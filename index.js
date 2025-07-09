f88f45b1118a0be1c3726f10c2febe68973be2b5
Apsarathapa <apsarathapa408@gmail.com>
2025-07-09 00:48:01 -0700

    Day 7: Added search route with Mongo + Regex, Added search input styling and JS Fetch logic

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const PORT = 3000;
+const Question = require('./models/questions.js');
+console.log('Question:', Question);
+
 
 app.use(express.json());
 
@@ -10,8 +13,19 @@ mongoose.connect('mongodb://127.0.0.1:27017/mydatabase')
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.error('MongoDB error:', err));
 
-// Use the routes
-app.use('/questions', require('./routes/questions'));
+
+//Adding search routes in Express
+app.get('/search', async (req, res) => {
+  const query = req.query.q || '';
+  try {
+    const questions = await Question.find({
+      title: { $regex: query, $options: 'i' }
+    });
+    res.status(200).json(questions);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 // Route: POST /submit (example route for form submissions)
 app.post('/submit', (req, res) => {
@@ -37,7 +51,32 @@ app.get('/services', (req, res) => {
   res.send('Here are our Services!');
 });
 
+
+app.get('/', (req, res) => {
+  res.send('Quora Backend Home');
+});
+
+app.get('/searchtest', (req, res) => {
+  res.send('Search test route works!');
+});
+
+
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
+
+
+
+
+// // Sample POST route for testing
+// app.post('/questions', async (req, res) => {
+//   try {
+//     const question = new Question(req.body);
+//     const saved = await question.save();
+//     res.status(201).json(saved);
+//   } catch (e) {
+//     res.status(400).json({ message: e.message });
+//   }
+// });
--- routes/questions.js
+++ routes/questions.js
@@ -1,7 +1,33 @@
+console.log('questions.js routes loaded'); 
+
 // routes/questions.js
 const express = require('express');
 const router = express.Router();
 const Question = require('../models/questions');
+
+// GET all questions
+router.get('/', async (req, res) => {
+  try {
+    const questions = await Question.find();
+    res.status(200).json(questions);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// SEARCH questions
+// router.get('/search', async (req, res) => {
+//   const query = req.query.q || '';
+//   console.log("Search endpoint hit. Query:", query);
+//   try {
+//     const questions = await Question.find({
+//       title: { $regex: query, $options: 'i' }
+//     });
+//     res.status(200).json(questions);
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// });
 
 // POST route to add a question
 router.post('/', async (req, res) => {
@@ -12,16 +38,6 @@ router.post('/', async (req, res) => {
     res.status(201).json(saved);
   } catch (error) {
     res.status(400).json({ message: error.message });
-  }
-});
-
-// GET all questions
-router.get('/', async (req, res) => {
-  try {
-    const questions = await Question.find();
-    res.status(200).json(questions);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
   }
 });
 
